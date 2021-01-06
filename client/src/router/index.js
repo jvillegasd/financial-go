59dc9a206149682a8503f239f13ed25cc8e65907
C9-LinkRs <linkrsfan@gmail.com>
2021-01-06 03:16:20 +0100

    Tailwindcss installed and set up

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Landing from '../views/Landing.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
---
+++ client/src/views/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="landing">
+    <div id="navbar">
+      <button id="about_btn">About</button>
+      <button class="bordered">Sign in</button>
+    </div>
+    <div id="text">
+      <h1>Financial Go</h1>
+      <h2>The easiest way to manage your budget.</h2>
+      <button class="non-bordered">Start saving</button>
+    </div>
+    <div id="logo">
+      
+    </div>
+  </div>
+</template>
