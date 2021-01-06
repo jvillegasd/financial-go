12bad1a4315e7be53c0c80580c164f616c9ce094
C9-LinkRs <linkrsfan@gmail.com>
2021-01-06 23:14:57 +0100

    Main colors added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,14 @@
+const tinycolor = require('tinycolor2');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      
+      "blue": "#005EEE",
+      "blue-80": tinycolor("#005EEE").setAlpha(0.8).toRgbString(),
+      "black": "#1C202E",
+      "grey-bg": "#F4F4F4",
     },
     fontFamily: {
       regular: ["GT Walsheim Pro Regular", "sans-serif"],
