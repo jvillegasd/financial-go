3154ed0e22e8a470d1dd38d92a41a719df51736c
C9-LinkRs <linkrsfan@gmail.com>
2021-01-08 00:28:59 +0100

    Content text on lading page incrieased left alignment

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="landing">
-    
+
     <div id="navbar" class="w-full Flex flex-row">
       <button id="btn-about" class="absolute right-64">About</button>
       <button id="btn-signin" class="absolute right-14">Sign in</button>
     </div>
 
     <div class="Flex flex-row">
-      <div id="text" class="absolute inset-y-96 left-24">
+      <div id="text" class="absolute inset-y-96 left-32">
         <h1 class="font-regular text-6xl">Financial Go</h1>
         <p id="content">The easiest way to manage your budget.</p>
         <button id="btn-signup">Start saving</button>
