6c830ecde5c0af908e1662a7efed3c06539c740e
C9-LinkRs <linkrsfan@gmail.com>
2021-01-08 00:25:20 +0100

    landing page done

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="landing">
-    <div id="navbar" class="w-full Flex flex-row right">
-      <button id="btn-about">About</button>
-      <button id="btn-signin">Sign in</button>
+    
+    <div id="navbar" class="w-full Flex flex-row">
+      <button id="btn-about" class="absolute right-64">About</button>
+      <button id="btn-signin" class="absolute right-14">Sign in</button>
     </div>
-    <div id="text" class="Flex flex-col justify-center h-full">
-      <h1 class="font-regular text-6xl">Financial Go</h1>
-      <p id="content">The easiest way to manage your budget.</p>
-      <button id="btn-signup">Start saving</button>
+
+    <div class="Flex flex-row">
+      <div id="text" class="absolute inset-y-96 left-24">
+        <h1 class="font-regular text-6xl">Financial Go</h1>
+        <p id="content">The easiest way to manage your budget.</p>
+        <button id="btn-signup">Start saving</button>
+      </div>
+
+      <div id="image-div" class="absolute inset-y-28 right-0">
+        <img id="image" src="../assets/logo.svg">
+      </div>
     </div>
-    <div id="image">
-      <img src="../assets/logo.svg">
-    </div>
+
   </div>
 </template>
 
