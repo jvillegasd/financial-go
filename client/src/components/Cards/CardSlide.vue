92427dffb437a3f3a06c2f66d5bcc00ab8a1cdb5
C9-LinkRs <linkrsfan@gmail.com>
2021-02-18 01:37:15 +0100

    Card slide component almost done; Have to dinamize the slider

--- client/src/components/Cards/CardSlide.vue
+++ client/src/components/Cards/CardSlide.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="wallet bg-grad-pb rounded-2xl space-y-2">
+  <div class="flex flex-col items-center">
+    <div class="wallet bg-grad-pb shadow-md rounded-2xl space-y-2 z-30">
       <div class="wallet-header flex justify-between items-center pl-8 pr-6 pt-4">
 
         <div class="text-left">
@@ -9,7 +9,7 @@
         </div>
 
         <div>
-          <img class="card_slide_img block mx-auto" src="../../assets/icons/bank.svg">
+          <img class="card-slide-img block mx-auto" src="../../assets/icons/bank.svg">
         </div>
 
       </div>
@@ -21,7 +21,7 @@
         </div>
 
         <svg class="pt-1" height="27.98" width="10">
-          <line x1="0" y1="0" x2="0" y2="27.98" opacity="0.4" style="stroke: rgb(250, 250, 250); stroke-width: 2;" />
+          <line x1="0" y1="0" x2="0" y2="27.98" opacity="0.3" style="stroke: rgb(250, 250, 250); stroke-width: 2;" />
         </svg>
 
         <div class="text-left">
@@ -30,12 +30,21 @@
         </div>
       </div>
     </div>
+
+    <div class="flex flex-col items-center -mt-5">
+      <div class="rounded-2xl bg-white shadow-md z-20" style="height: 37px; width: 307px;"></div>
+      <div class="rounded-2xl bg-white shadow-md z-10 -mt-3" style="height: 27px; width: 290px;"></div>
+      <rounded-btn class="z-20 -mt-5"></rounded-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import RoundedBtn from "../../components/Buttons/Rounded_btn.vue";
+
 export default {
-  name: "wallet-slide"
+  name: "wallet-slide",
+  components: {RoundedBtn}
 }
 </script>
 
@@ -48,9 +57,9 @@ export default {
     color: rgba(250, 250, 250, 0.5);
   }
   .bg-grad-pb {
-    background: linear-gradient(0deg, rgba(117,98,255,1) 0%, rgba(51,201,255,1) 100%);
+    background: linear-gradient(172deg, rgba(51,201,255,1) 0%, rgba(117,98,255,1) 100%);
   }
-  .card_slide_img {
+  .card-slide-img {
     opacity: 0.1;
     width: 80px;
     height: 79.81px;
