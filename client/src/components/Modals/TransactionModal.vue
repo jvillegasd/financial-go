29db883255018871bb9a8ec422e859a800bc40fa
C9-LinkRs <linkrsfan@gmail.com>
2021-05-11 19:37:31 +0200

    Grey select box almost done; have to create emit event

--- client/src/components/Modals/TransactionModal.vue
+++ client/src/components/Modals/TransactionModal.vue
@@ -4,49 +4,55 @@
     <div>
       <div class="fixed overflow-x-hidden overflow-y-auto inset-0 z-40 flex justify-center items-center" >
         <div class="relative mx-auto w-auto max-w-2xl">
-          <div class="bg-white w-20 modal rounded-2xl">
+          <div class="bg-white w-20 modal rounded-2xl flex flex-col items-center">
+            
             <p class="text-center font-medium text-xl">New transaction</p>
+
             <Textbox
               label="Title"
               :width=245
               :labelFontSize=20
               :inputFontSize=25
-              :isPassword=false
-              class="mx-auto">
+              :isPassword=false>
             </Textbox>
 
-            <div class="text-left">
-              <label class="font-regular block label-select">Type</label>
-              <select name="select" class="truncate rounded-xl pl-14 select-wallet block appearance-none outline-none bg-grey-select font-medium rounded leading-tight">
-                <option value="value1">Value 1</option>
-                <option value="value2" selected>Value 2</option>
-                <option value="value3">Value 3</option>
-              </select>
-            </div>
+            <select-transaction
+              title="Type"
+              :options="transaction_types"
+            ></select-transaction>
 
             <Textbox
               label="Amount"
               :width=245
               :labelFontSize=20
               :inputFontSize=25
-              :isPassword=false
-              class="mx-auto">
+              :isPassword=false>
             </Textbox>
+
             <Textbox
               label="Date"
               :width=245
               :labelFontSize=20
               :inputFontSize=25
-              :isPassword=false
-              class="mx-auto">
+              :isPassword=false>
             </Textbox>
+
+            <div>
+              <select-transaction
+              title="Category"
+              :options="categories"
+              :width=200>
+              </select-transaction>
+            </div>
+
             <BlueButton
               class="font-medium"
               title="Create transaction"
               :width=303
               :height=46
-              :fontSize=18
-            ></BlueButton>
+              :fontSize=18>
+            </BlueButton>
+            
           </div>
         </div>
       </div>
@@ -60,16 +66,38 @@
 <script>
 import Textbox from '../../components/Textbox/Textbox';
 import BlueButton from '../../components/Buttons/Blue_btn.vue';
+import SelectTransaction from '../../components/Selectbox/SelectTransaction.vue';
 
 export default {
   name: "transaction-modal",
   components: {
     Textbox,
-    BlueButton
+    BlueButton,
+    SelectTransaction
   },
   data() {
     return {
       show: false,
+      transaction_types: [
+        {
+          value: 'income',
+          text: 'Income'
+        },
+        {
+          value: 'outcome',
+          text: 'Outcome'
+        },
+        {
+          value: 'recurrent',
+          text: 'Recurrent'
+        }
+      ],
+      categories: [
+        {
+          value: 'entertainment',
+          text: 'Entertainment'
+        },
+      ]
     };
   },
   methods: {
@@ -81,9 +109,5 @@ export default {
   .modal {
     width: 486px;
     height: 652px;
-  }
-  .label-select {
-    color: rgb(174,177,184);
-    font-size: 20px;
   }
 </style>
---
+++ client/src/components/Selectbox/SelectTransaction.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="relative select-transaction-div" :style="cssVars">
+    <div class=" mt-1 relative">
+
+
+      <label class="text-left  font-regular block label-select">{{ title }}</label>
+      <select v-model="selected_option" name="select" class="truncate rounded-xl pl-5 select-wallet block appearance-none outline-none bg-grey-select font-bold text-xl rounded leading-tight ">
+        <option
+        v-for="option in options"
+        v-bind:key="option.text"
+        v-bind:value="option.value"
+        >
+          {{ option.text }}
+        </option>
+      </select>
+
+      <div class="absolute flex inset-y-0 items-center pr-4 pt-7 right-0 pointer-events-none">
+        <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M18.6637 9.66375L13.5 14.8162L8.33625 9.66375L6.75 11.25L13.5 18L20.25 11.25L18.6637 9.66375Z" fill="#1D1D1D" fill-opacity="0.6"/>
+        </svg>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "select-transaction",
+  props: {
+    options: Array,
+    title: String,
+    width: {
+      type: Number,
+      default: 160
+    }
+  },
+  data () {
+    return {
+      selected_option: null
+    }
+  },
+  mounted () {
+    if (this.options.length) {
+      this.selected_option = this.options[0].value;
+    }
+  },
+  computed: {
+    cssVars () {
+      return {
+        '--width': this.width + 'px'
+      };
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .select-transaction-div {
+    width: var(--width);
+  }
+  .label-select {
+    color: rgb(174,177,184);
+    font-size: 20px;
+  }
+  .select-wallet {
+    width: var(--width);
+    height: 40px;
+  }
+</style>
