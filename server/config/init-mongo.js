6ecb320caac701895df8c9ed84a55058fd55d178
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-24 22:57:50 -0500

    feat: :sparkles: Starting from zero

--- server/config/init-mongo.js
+++
@@ -1,6 +0,0 @@
-db = db.getSiblingDB('database-financial-go');
-
-db.createCollection('users');
-db.createCollection('cards');
-db.createCollection('invoices');
-db.createCollection('transactions');
