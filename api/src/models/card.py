1dc009f0944804da47b501f133850dcba63fb86e
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-25 20:37:38 -0500

    feat: :sparkles: Removed unused code

--- api/src/models/card.py
+++ api/src/models/card.py
@@ -10,14 +10,3 @@ class Card(BaseDocument):
     owner_uuid = UUIDField(binary=False)
     title = StringField(required=True)
     amount = FloatField(default=0.0)
-
-    def add_transaction(self, new_transaction: Transaction):
-        """
-          Save a reference of Transaction to current user.
-          Args:
-            - new_transaction: Transaction = Transaction
-            to be referenced to user.
-        """
-
-        new_transaction.owner_uuid = self.uuid
-        new_transaction.save()
