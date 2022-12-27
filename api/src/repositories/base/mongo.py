18101262286d2978315932a30f71f08c02eca491
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-27 00:16:29 -0500

    feat: :sparkles: pagination implemented

--- api/src/repositories/base/mongo.py
+++ api/src/repositories/base/mongo.py
@@ -1,12 +1,18 @@
-from typing import TypeVar
+from typing import TypeVar, Optional
+from src.models.base import BaseDocument
 from mongoengine.queryset import QuerySet
+from src.schemas.model_pagination import (
+    ModelPagination,
+    PaginationCursor,
+    QueryParamPagination
+)
 from src.schemas.filter import FilterSchema
 from pymongo.client_session import ClientSession
 from src.constants import ALLOWED_QUERY_OPERATORS
 from src.interfaces.repository import IRepository
 from src.errors.filter import InvalidFilterOperator, InvalidFilterColumn
 
-ModelType = TypeVar('ModelType')
+ModelType = TypeVar('ModelType', bound=BaseDocument)
 
 
 class MongoEngineRepository(IRepository[ModelType, QuerySet]):
@@ -18,6 +24,44 @@ class MongoEngineRepository(IRepository[ModelType, QuerySet]):
     ):
         self.model: ModelType = model
         self.session: ClientSession = session
+
+    def apply_pagination(
+        self,
+        query: QuerySet,
+        paginate: Optional[QueryParamPagination] = None
+    ) -> ModelPagination:
+        import datetime
+
+        per_page: int = 10
+        result: list[ModelType]
+        cursor_timestamp: Optional[float] = None
+        if paginate:
+            per_page = paginate.per_page or 10
+            cursor_timestamp = paginate.next_cursor
+
+        if cursor_timestamp:
+            cursor_timestamp = float(cursor_timestamp)
+            cursor_date = datetime.datetime.fromtimestamp(
+                cursor_timestamp
+            )
+            result = query.filter(
+                created_at__le=cursor_date
+            ).order_by('-created_at').limit(per_page).all()
+        else:
+            result = query.order_by('-created_at').limit(per_page).all()
+
+        next_cursor: Optional[int] = None
+        if result:
+            next_cursor = result[-1].created_at.timestamp()
+
+        return ModelPagination(
+            records=result,
+            cursor=PaginationCursor(
+                prev=cursor_timestamp,
+                next=next_cursor,
+                per_page=per_page
+            )
+        )
 
     def _apply_filters(
         self,
