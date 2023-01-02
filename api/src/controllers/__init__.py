855d39062cfc107bfe35b01bf3edc54e67d491f0
Jvillegasd <johnnyvillegaslrs@gmail.com>
2023-01-01 20:59:30 -0500

    feat: auth controller implemented

--- api/src/controllers/__init__.py
+++ api/src/controllers/__init__.py
@@ -1,1 +1,2 @@
 from src.controllers.home import home_api
+from src.controllers.auth import auth_api
--- api/src/controllers/auth.py
+++ api/src/controllers/auth.py
@@ -5,10 +5,12 @@ from src.services.user import UserService
 from middlewares.schemas import validate_body
 from flask import request, Blueprint, abort
 from src.errors.user import UserNotFoundError, UserBadCredentials
+from src.unit_of_work.mongo import MongoUnitOfWork
 
 auth_api = Blueprint('Authorization', __name__)
 auth_service = AuthService()
 user_service = UserService()
+uow = MongoUnitOfWork()
 
 
 @auth_api.post('/')
@@ -16,7 +18,10 @@ user_service = UserService()
 def create_auth_token():
     body = request.get_json()
     try:
-        user: User = user_service.get_user_by_email(body['email'])
+        user: User = user_service.get_user_by_email(
+            body['email'],
+            uow
+        )
         if not user.check_password(body['password']):
             raise UserBadCredentials
         auth_token = auth_service.create_auth_token(user)
